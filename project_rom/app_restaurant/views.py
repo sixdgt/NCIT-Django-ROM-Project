e0dca0490ece5226ca7852e2adfa5f88b3821abc
sixdgt <c4crypt@gmail.com>
2026-01-04 14:56:47 +0545

    Django Messages

--- project_rom/app_restaurant/views.py
+++ project_rom/app_restaurant/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from app_restaurant.forms import MenuCreateForm
 from app_restaurant.models import Menu, Category
 from django.db.models import Q
+from django.contrib import messages
+
 # Create your views here.
 def menu_list(request):
     if request.method == "GET" and "search" in request.GET:
@@ -30,8 +32,10 @@ def menu_create(request):
         db_data = MenuCreateForm(request_data)
         if db_data.is_valid():
             db_data.save()
+            messages.add_message(request, messages.SUCCESS, "Menu added successfully!!")
             return redirect("menu.list")
         else:
+            messages.add_message(request, messages.ERROR, "Something went wrong!!")
             return redirect("menu.create")
     else:
         # if the request method is GET or page request then loading the form
@@ -46,8 +50,10 @@ def menu_edit(request, pk):
         db_data = MenuCreateForm(request_data, instance=menu)
         if db_data.is_valid():
             db_data.save()
+            messages.add_message(request, messages.SUCCESS, "Menu udpated successfully!")
             return redirect("menu.list")
         else:
+            messages.add_message(request, messages.ERROR, "Something went wrong!")
             return redirect("menu.edit", pk=pk)
     else:
         context = { "form": form }
@@ -64,6 +70,8 @@ def menu_delete(request, pk):
     try:
         menu_data = Menu.objects.get(id=pk)
         menu_data.delete()
+        messages.add_message(request, messages.ERROR, "Menu deleted successfully!")
         return redirect("menu.list")
     except Menu.DoesNotExist:
-        return HttpResponse("Menu item not found", status=404)
+        messages.add_message(request, messages.WARNING, "Menu not found")
+        return redirect("menu.list")
--- project_rom/project_rom/settings.py
+++ project_rom/project_rom/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
+
+# for bootstrap danger class
+MESSAGE_TAGS = {
+    messages.ERROR: "danger"
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
